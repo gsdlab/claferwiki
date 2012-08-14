69caee6ef7ccaad3091094462ad507a9fa9b989a
Chris Walker <cwwalker@uwaterloo.ca>
2012-08-14 18:02:08 -0400

    Improved formatting of .links

--- plugins/ClaferWriter.hs
+++ plugins/ClaferWriter.hs
@@ -46,8 +46,8 @@ summary withGraph withStats withLinks = do
                     _ <- readProcessWithExitCode "dot" ["-Tsvg", "-o", "static/clafer/summary.svg"] output
                     out <- readFile "static/clafer/summary.svg"
                     return $ RawBlock "html" ((if withGraph then out else "") ++ (if withGraph && withStats then "<br>\n" else "") ++ 
-                                                (if withStats then "Module Statistics:<br>\n<span class=\"summary\">" ++ unlines (map (++"<br>") (lines stats)) else "") ++
-                                                if withLinks then "</class><br>\nModule Downloads: <a href=clafer/" ++ fileName ++ ".cfr>[.cfr]</a> <a href=clafer/" ++ fileName ++ ".html>[.html]</a>" else "")
+                                                (if withStats then "Module Statistics:<br>\n" ++ unlines (map (++"<br>") (lines stats)) else "") ++ (if withLinks && (withStats || withGraph) then "<br>\n" else "")
+                                                if withLinks then "Module Downloads: <a href=clafer/" ++ fileName ++ ".cfr>[.cfr]</a> <a href=clafer/" ++ fileName ++ ".html>[.html]</a>" else "")
                 Left err -> return $ RawBlock "html" ("<pre>\n" ++ concatMap handleErr err ++ "\n</pre>")
                   where handleErr (ClaferErr msg) = "Clafer encountered an error: " ++ msg
                         handleErr (ParseErr ErrPos{modelPos = Pos l c} msg) = "Clafer encountered a parse error at line " ++ show l ++ ", column " ++ show c ++ " " ++ msg
