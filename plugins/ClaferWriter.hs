bcc13a17a19af0543b4296634dc55aaa03421232
Chris Walker <cwwalker@uwaterloo.ca>
2012-08-14 17:25:24 -0400

    Wiki supports .graph, .stats, and .links
    .summary is a shorthand for .graph .stats .links

--- plugins/ClaferWriter.hs
+++ plugins/ClaferWriter.hs
@@ -14,17 +14,21 @@ plugin :: Plugin
 plugin = mkPageTransformM readBlock
 
 readBlock :: Block -> PluginM Block
-readBlock (CodeBlock (id, classes, namevals) contents)
-  | "clafer" `elem` classes && (not $ "summary" `elem` classes) = liftIO $ do
+readBlock block@(CodeBlock (id, classes, namevals) contents)
+  | "clafer" `elem` classes && "summary" `elem` classes = summary True True True
+  | "clafer" `elem` classes && "graph" `elem` classes || "stats" `elem` classes || "links" `elem` classes
+    = summary ("graph" `elem` classes)  ("stats" `elem` classes) ("links" `elem` classes)
+  | "clafer" `elem` classes = liftIO $ do
     contents <- getBlock
     return $ RawBlock "html" ("<div class=\"code\">" ++ contents ++ "</div>")
-  | "clafer" `elem` classes && "summary" `elem` classes = do
+readBlock x = return x
+
+summary withGraph withStats withLinks = do
       let args = defaultClaferArgs{mode=Just Graph, keep_unused=Just True}
-      cfg <- askConfig
       liftIO $ do
       fileName <- readFile "static/clafer/name.txt"
       fileExists <- doesFileExist $ "static/clafer/" ++ fileName ++ ".cfr"
-      if fileExists--file may not exist if compilation failed
+      if fileExists--file may not exist if an error occurred
       then do content <- readFile $ "static/clafer/" ++ fileName ++ ".cfr"
               graph <- runClaferT args $ do
                   addModuleFragment content
@@ -41,13 +45,13 @@ readBlock (CodeBlock (id, classes, namevals) contents)
                                        mappingToAlloy = Nothing } -> do
                     _ <- readProcessWithExitCode "dot" ["-Tsvg", "-o", "static/clafer/summary.svg"] output
                     out <- readFile "static/clafer/summary.svg"
-                    return $ RawBlock "html" (out ++ "<br>\nModule Statistics:<br>\n<span class=\"summary\">" ++ unlines (map (++"<br>") (lines stats)) ++
-                                                    "</class><br>\nModule Downloads: <a href=clafer/" ++ fileName ++ ".cfr>[.cfr]</a> <a href=clafer/" ++ fileName ++ ".html>[.html]</a>")
+                    return $ RawBlock "html" ((if withGraph then out else "") ++ (if withGraph && withStats then "<br>\n" else "") ++ 
+                                                (if withStats then "Module Statistics:<br>\n<span class=\"summary\">" ++ unlines (map (++"<br>") (lines stats)) else "") ++
+                                                if withLinks then "</class><br>\nModule Downloads: <a href=clafer/" ++ fileName ++ ".cfr>[.cfr]</a> <a href=clafer/" ++ fileName ++ ".html>[.html]</a>" else "")
                 Left err -> return $ RawBlock "html" ("<pre>\n" ++ concatMap handleErr err ++ "\n</pre>")
                   where handleErr (ClaferErr msg) = "Clafer encountered an error: " ++ msg
                         handleErr (ParseErr ErrPos{modelPos = Pos l c} msg) = "Clafer encountered a parse error at line " ++ show l ++ ", column " ++ show c ++ " " ++ msg
       else return $ RawBlock "html" "<!-- # SUMMARY /-->"
-readBlock x = return x
 
 --this is added so that it won't break if the wiki contains code blocks with no headers
 first [] = []
