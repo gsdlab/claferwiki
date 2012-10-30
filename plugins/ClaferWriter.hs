b141adb20c15a3d71424d6f8b6738e460ff4f3c6
Michal Antkiewicz <mantkiew@gsd.uwaterloo.ca>
2012-10-30 15:18:16 -0400

    graph scrolling and improved generation
    fixed style

--- plugins/ClaferWriter.hs
+++ plugins/ClaferWriter.hs
@@ -31,35 +31,64 @@ readBlock x = return x
 
 withGraph Nothing = False
 withGraph _       = True
-						
+                                                
 summary graphMode withStats withLinks = do
-      let args = defaultClaferArgs{mode=(if withGraph graphMode then graphMode else Just Graph), keep_unused=Just True}
-      liftIO $ do
-      fileExists <- doesFileExist "static/clafer/name.txt"
-      if fileExists--file may not exist if an error occurred
-      then do fileName <- readFile "static/clafer/name.txt"
-              content <- readFile $ "static/clafer/" ++ fileName ++ ".cfr"
-              graph <- runClaferT args $ do
-                  addModuleFragment content
-                  parse
-                  compile
-                  generate
-              case graph of
-                Right CompilerResult { extension = ext,
-                                       outputCode = output,
-                                       statistics = stats } -> do
-                    _ <- readProcessWithExitCode "dot" ["-Tsvg", "-o", "static/clafer/summary.svg"] output
-                    out <- readFile "static/clafer/summary.svg"
-                    return $ RawBlock "html" ((if (withGraph graphMode) then out else "") ++ (if (withGraph graphMode) && withStats then "<br>\n" else "") ++ 
-                                                (if withStats then "Module Statistics:<br>\n" ++ unlines (map (++"<br>") (lines stats)) else "") ++ (if withLinks && (withStats || (withGraph graphMode)) then "<br>\n" else "") ++
-                                                if withLinks then "Module Downloads: <a href=clafer/" ++ fileName ++ ".cfr>[.cfr]</a> <a href=clafer/" ++ fileName ++ ".html>[.html]</a>" else "")
-                        
-                Left err -> return $ RawBlock "html" ("<pre>\n" ++ (concatMap handleErr err) ++ "\n</pre>")
-                  where handleErr (ClaferErr msg) = "Clafer encountered an error: " ++ msg
-                        handleErr (ParseErr ErrPos{modelPos = Pos l c} msg) = "Clafer encountered a parse error at line " ++ show l ++ ", column " ++ show c ++ " " ++ msg
-                        handleErr (SemanticErr ErrPos{modelPos = Pos l c} msg) = "Clafer encountered a compilation error at line " ++ show l ++ ", column " ++ show c ++ " " ++ msg						
-      else return $ RawBlock "html" "Clafer error: <span class=\"error\">No clafer model found</span>"
+        let argsWithoutRefs = defaultClaferArgs{mode=(if withGraph graphMode then graphMode else Just Graph), keep_unused=Just True, show_references=Just False}
+        let argsWithRefs = defaultClaferArgs{mode=(if withGraph graphMode then graphMode else Just Graph), keep_unused=Just True, show_references=Just True}
+        liftIO $ do
+        fileExists <- doesFileExist "static/clafer/name.txt"
+        if fileExists--file may not exist if an error occurred
+        then do 
+                fileName <- readFile "static/clafer/name.txt"
+                content <- readFile $ "static/clafer/" ++ fileName ++ ".cfr"
+                graphWithoutRefs <- runClaferT argsWithoutRefs $ do
+                        addModuleFragment content
+                        parse
+                        compile
+                        generate
+                graphWithRefs <- runClaferT argsWithRefs $ do
+                        addModuleFragment content
+                        parse
+                        compile
+                        generate     
+                case graphWithoutRefs of
+                        Right CompilerResult { extension = ext,
+                                               outputCode = dotWithoutRefs,
+                                               statistics = stats } -> do
+                                case graphWithRefs of
+                                        Right CompilerResult { outputCode = dotWithRefs } -> do
+                                                (_, unflattenedDotWithoutRefs, _) <- readProcessWithExitCode "unflatten" [ "-l 1000" ] dotWithoutRefs
+                                                (_, unflattenedDotWithRefs, _) <- readProcessWithExitCode "unflatten" [ "-l 1000" ] dotWithRefs
+                                                (_, outWithoutRefs, _) <- readProcessWithExitCode "dot" [ "-Tsvg" ] unflattenedDotWithoutRefs
+                                                (_, outWithRefs, _) <- readProcessWithExitCode "dot" [ "-Tsvg" ] unflattenedDotWithRefs
+                                                return $ RawBlock "html" ((if (withGraph graphMode) then (createGraphWithToggle outWithoutRefs outWithRefs) else "") ++ 
+                                                                          (if (withGraph graphMode) && withStats then "<br>\n" else "") ++ 
+                                                                          (if withStats then "Module Statistics:<br>\n" ++ unlines (map (++"<br>") (lines stats)) else "") ++ 
+                                                                          (if withLinks && (withStats || (withGraph graphMode)) then "<br>\n" else "") ++
+                                                                          (if withLinks then "Module Downloads: <a href=/static/clafer/" ++ fileName ++ ".cfr>[.cfr]</a> <a href=/static/clafer/" ++ fileName ++ ".html>[.html]</a>" else ""))
+                                        Left err -> return $ RawBlock "html" ("<pre>\n" ++ (concatMap handleErr err) ++ "\n</pre>")
+                        Left err -> return $ RawBlock "html" ("<pre>\n" ++ (concatMap handleErr err) ++ "\n</pre>")
+        else return $ RawBlock "html" "Clafer error: <span class=\"error\">No clafer model found</span>"
 
+handleErr (ClaferErr msg) = "Clafer encountered an error: " ++ msg
+handleErr (ParseErr ErrPos{modelPos = Pos l c} msg) = "Clafer encountered a parse error at line " ++ show l ++ ", column " ++ show c ++ " " ++ msg
+handleErr (SemanticErr ErrPos{modelPos = Pos l c} msg) = "Clafer encountered a compilation error at line " ++ show l ++ ", column " ++ show c ++ " " ++ msg                                                
+
+
+createGraphWithToggle outWithoutRefs outWithRefs = unlines [
+    "<div id=\"graphWithoutRefs\" style=\"display:block;width:100%;border:solid lightgray 1px;overflow-x:auto;\" ondblclick=\"" ++ showRefs  ++ "\">",
+    outWithoutRefs, 
+    "</div>",
+    "<div id=\"graphWithRefs\" style=\"display:none;width:100%;border:solid lightgray 1px;overflow-x:auto;\" ondblclick=\"" ++ hideRefs  ++ "\">",
+    outWithRefs, 
+    "</div>" ]
+
+showRefs =
+  "var gwr=document.getElementById('graphWithRefs'); gwr.style.display='block'; gwr.scrollLeft=this.scrollLeft; this.style.display='none';"
+  
+hideRefs = 
+  "var gwor=document.getElementById('graphWithoutRefs'); gwor.style.display='block'; gwor.scrollLeft=this.scrollLeft;this.style.display='none';"
+    
 --this is added so that it won't break if the wiki contains code blocks with no headers
 first [] = []
 first (x:xs) = x
