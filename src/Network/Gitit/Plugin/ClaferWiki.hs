bff14eaaa32eb61992100a3f878053c223a1cba1
Michał Antkiewicz <michal.antkiewicz@uwaterloo.ca>
2023-08-25 14:46:17 -0400

    migrate to using Lazy Text Builder

--- src/Network/Gitit/Plugin/ClaferWiki.hs
+++ src/Network/Gitit/Plugin/ClaferWiki.hs
@@ -1,5 +1,6 @@
 {-
- Copyright (C) 2012-2014 Michal Antkiewicz, Chris Walker <http://gsd.uwaterloo.ca>
+ Copyright 2012-2019 Generative Software Development Lab <http://gsd.uwaterloo.ca>
+ Copyright 2019-2023 Intelligent Systems Development Lab <https://uwaterloo.ca/wise-lab/>
 
  Permission is hereby granted, free of charge, to any person obtaining a copy of
  this software and associated documentation files (the "Software"), to deal in
@@ -28,19 +29,21 @@ module Network.Gitit.Plugin.ClaferWiki (plugin) where
 
 import Network.Gitit.Interface
 
-import Control.Monad
-import Control.Monad.Trans.State
-import Data.List
-import Data.List.Split
+import Control.Monad (unless)
+import Control.Monad.Trans.State (evalState, get, gets, put, State)
+import Data.List (intercalate, nub)
+import Data.List.Split (splitOn)
 import qualified Data.Map as Map
 import Data.String.Utils (replace)
 import qualified Data.Text as T
+import Data.Text.Lazy (toStrict)
+import Data.Text.Lazy.Builder (Builder,  fromString, toLazyText, singleton)
 import Network.BSD (getHostName)
 import System.Directory (createDirectoryIfMissing, doesFileExist)
 import System.Process (readProcessWithExitCode)
 
 import Language.Clafer
-import Language.Clafer.Css as Css
+import Language.Clafer.Css as Css (css, header)
 import Language.Clafer.Generator.Html (highlightErrors)
 
 -- | claferWiki collects Clafer code from .clafer code blocks, renders as HTML and graph,
@@ -67,6 +70,7 @@ claferWiki pandoc = do
         allCompilationResults = compileFragments fragments claferModes
 
         htmlCode = extractOutput allCompilationResults Html
+        htmlCodeFragments :: [String]
         htmlCodeFragments = splitOn "\n<!-- # FRAGMENT /-->\n" htmlCode
 
         stats = maybe "No model." statistics $ extractCompilerResult allCompilationResults Html
@@ -80,8 +84,8 @@ claferWiki pandoc = do
     (_, svgGraphWithRefs, _) <- liftIO $ readProcessWithExitCode "dot" [ "-Tsvg" ] $ changeTransparentToLightGray dotGraph
     (_, svgCVLGraph, _) <- liftIO $ readProcessWithExitCode "dot" [ "-Tsvg" ] dotCVLGraph
 
-        -- using the WikiEnv as state, replace clafer code blocks with appropriate results:
-        -- html rendering of clafer code, graph rendering, download links, ide, configurator, and visualizer buttons
+    -- using the WikiEnv as state, replace clafer code blocks with appropriate results:
+    -- html rendering of clafer code, graph rendering, download links, ide, configurator, and visualizer buttons
     let
         initialWikiEnv = WikiEnv {
                             we_fileName = pageName,
@@ -140,6 +144,7 @@ claferWiki pandoc = do
                 Nothing -> "Error: No " ++ show claferMode ++ " output."
         extractOutput (Left err) _ = highlightErrors fragmentedModel err
 
+        selfContained :: String -> String
         selfContained htmlCode =
             concat [
                 Css.header,
@@ -172,16 +177,16 @@ replaceClaferWikiBlocks (CodeBlock (_, [ "clafer" ], _) _) = do
     case we_htmlCodeFragments wikiEnv of
         (fragment:fragments) -> do
             put $ wikiEnv { we_htmlCodeFragments = fragments }
-            return $ RawBlock "html" ("<div class=\"code\">" <> T.pack fragment <> "</div>")
+            return $ RawBlock "html" $ toStrict . toLazyText $ "<div class=\"code\">" <> fromString fragment <> "</div>"
         []                   -> return $ Plain []
 
 replaceClaferWikiBlocks (CodeBlock (_, [ "clafer", "links" ], _) _) = do
     fileName <- gets we_fileName
-    return $ RawBlock "html" $ renderLinks (T.pack fileName)
+    return $ RawBlock "html" $ toStrict . toLazyText $ renderLinks fileName
 
 replaceClaferWikiBlocks (CodeBlock (_, [ "clafer", "stats" ], _) _) = do
     stats <- gets we_stats
-    return $ RawBlock "html" $ renderStats (T.pack stats)
+    return $ RawBlock "html" $ toStrict . toLazyText $ renderStats stats
 
 replaceClaferWikiBlocks (CodeBlock (_, [ "clafer", "graph" ], _) _) = do
     wikiEnv <- get
@@ -189,15 +194,15 @@ replaceClaferWikiBlocks (CodeBlock (_, [ "clafer", "graph" ], _) _) = do
     svgGraphWithRefs <- gets we_svgGraphWithRefs
     svgGraphWithoutRefs <- gets we_svgGraphWithoutRefs
     put $ wikiEnv { we_graphNo = graphNo + 1 }
-    return $ RawBlock "html" $ renderGraphWithToggle (T.pack svgGraphWithoutRefs) (T.pack svgGraphWithRefs) graphNo
+    return $ RawBlock "html" $ toStrict . toLazyText $ renderGraphWithToggle svgGraphWithoutRefs svgGraphWithRefs graphNo
 
 replaceClaferWikiBlocks (CodeBlock (_, [ "clafer", "cvlGraph" ], _) _) =  do
     svgCVLGraph <- gets we_svgCVLGraph
-    return $ RawBlock "html" $ renderGraph (T.pack svgCVLGraph)
+    return $ RawBlock "html" $ toStrict . toLazyText $ renderGraph svgCVLGraph
 
 replaceClaferWikiBlocks (CodeBlock (_, [ "clafer", "cvlgraph" ], _) _) =  do
     svgCVLGraph <- gets we_svgCVLGraph
-    return $ RawBlock "html" $ renderGraph (T.pack svgCVLGraph)
+    return $ RawBlock "html" $ toStrict . toLazyText $ renderGraph svgCVLGraph
 
 replaceClaferWikiBlocks (CodeBlock (_, [ "clafer", "summary" ], _) _) =  do
     wikiEnv <- get
@@ -207,7 +212,7 @@ replaceClaferWikiBlocks (CodeBlock (_, [ "clafer", "summary" ], _) _) =  do
     svgGraphWithRefs <- gets we_svgGraphWithRefs
     svgGraphWithoutRefs <- gets we_svgGraphWithoutRefs
     put $ wikiEnv { we_graphNo = graphNo + 1 }
-    return $ RawBlock "html" $ renderSummary fileName stats svgGraphWithoutRefs svgGraphWithRefs graphNo
+    return $ RawBlock "html" $ toStrict . toLazyText $ renderSummary fileName stats svgGraphWithoutRefs svgGraphWithRefs graphNo
 
 replaceClaferWikiBlocks (CodeBlock (_, [ "clafer", "mooviz" ], _) _) =  do
     wikiEnv <- get
@@ -223,105 +228,105 @@ replaceClaferWikiBlocks (CodeBlock (_, [ "clafer", "ide" ], _) _) =  do
 
 replaceClaferWikiBlocks block = return block
 
-renderLinks :: T.Text -> T.Text
+-- new line builder
+nl :: Builder
+nl = singleton '\n'
+
+renderLinks :: String -> Builder
 renderLinks fileName =
-    "<div><b>Module Downloads:</b> | <a href=\"/clafer/" <>
-    fileName <>
-    ".cfr\">[.cfr]</a> | <a href=\"/clafer/" <>
-    fileName <>
-    ".html\">[.html]</a> |</div><br>\n"
+    "<div><b>Module Downloads:</b> | <a href=\"/clafer/" <> nl <>
+    fromString fileName <> nl <>
+    ".cfr\">[.cfr]</a> | <a href=\"/clafer/" <> nl <>
+    fromString fileName <> nl <>
+    ".html\">[.html]</a> |</div><br>\n" <> nl
 
-renderStats :: T.Text -> T.Text
+renderStats :: String -> Builder
 renderStats stats =
-    "<div><b>Module Statistics:</b> \n| " <>
-    T.intercalate " | " (T.lines stats) <>
-    " |</div><br>\n"
+    "<div><b>Module Statistics:</b> \n| " <> nl <>
+    fromString (intercalate " | " (lines stats)) <> nl <>
+    " |</div><br>\n" <> nl
 
+renderGraphWithToggle :: String           -> String        -> Int      -> Builder
+renderGraphWithToggle    svgGraphWithoutRefs svgGraphWithRefs graphNo   =
+    "<div id=\"" <> renderGraphId False graphNo <> "\" style=\"display:block;width:100%;border:solid lightgray 1px;overflow-x:auto;\" ondblclick=\"" <> renderShowRefs graphNo <> "\">" <> nl <>
+    fromString svgGraphWithoutRefs <> nl <>
+    "</div>" <> nl <>
+    "<div id=\"" <> renderGraphId True graphNo <>  "\" style=\"display:none;width:100%;border:solid lightgray 1px;overflow-x:auto;\" ondblclick=\"" <> renderHideRefs graphNo <> "\">" <> nl <>
+    fromString svgGraphWithRefs <> nl <>
+    "</div>" <> nl
 
-renderGraphWithToggle :: T.Text           -> T.Text        -> Int      -> T.Text
-renderGraphWithToggle    svgGraphWithoutRefs svgGraphWithRefs graphNo   = T.unlines [
-    "<div id=\"" <> renderGraphId False graphNo <>  "\" style=\"display:block;width:100%;border:solid lightgray 1px;overflow-x:auto;\" ondblclick=\"" <> renderShowRefs graphNo <> "\">",
-    svgGraphWithoutRefs,
-    "</div>",
-    "<div id=\"" <> renderGraphId True graphNo <>  "\" style=\"display:none;width:100%;border:solid lightgray 1px;overflow-x:auto;\" ondblclick=\"" <> renderHideRefs graphNo <> "\">",
-    svgGraphWithRefs,
-    "</div>" ]
+renderGraph :: String  -> Builder
+renderGraph    svgGraph = 
+    "<div style=\"display:block;width:100%;border:solid lightgray 1px;overflow-x:auto;\">" <> nl <>
+    fromString svgGraph <> nl <>
+    "</div>" <> nl
 
-renderGraph :: T.Text  -> T.Text
-renderGraph    svgGraph = T.unlines [
-    "<div style=\"display:block;width:100%;border:solid lightgray 1px;overflow-x:auto;\">",
-    svgGraph,
-    "</div>" ]
-
-renderShowRefs :: Int    -> T.Text
+renderShowRefs :: Int    -> Builder
 renderShowRefs    graphNo =
-  "var gwr=document.getElementById('" <> renderGraphId True graphNo <>  "'); gwr.style.display='block'; gwr.scrollLeft=this.scrollLeft; this.style.display='none';"
+  "var gwr=document.getElementById('" <> renderGraphId True graphNo <> "'); gwr.style.display='block'; gwr.scrollLeft=this.scrollLeft; this.style.display='none';"  <> nl
 
-renderHideRefs :: Int    -> T.Text
+renderHideRefs :: Int    -> Builder
 renderHideRefs    graphNo =
-  "var gwor=document.getElementById('" <> renderGraphId False graphNo <>  "'); gwor.style.display='block'; gwor.scrollLeft=this.scrollLeft;this.style.display='none';"
+  "var gwor=document.getElementById('" <> renderGraphId False graphNo <>  "'); gwor.style.display='block'; gwor.scrollLeft=this.scrollLeft;this.style.display='none';" <> nl
 
-renderGraphId :: Bool -> Int    -> T.Text
-renderGraphId    True    graphNo = T.pack $ "graphWithRefs" ++ show graphNo
-renderGraphId    False   graphNo = T.pack $ "graphWithoutRefs" ++ show graphNo
+renderGraphId :: Bool -> Int    -> Builder
+renderGraphId    True    graphNo = fromString $ "graphWithRefs" ++ show graphNo
+renderGraphId    False   graphNo = fromString $ "graphWithoutRefs" ++ show graphNo
 
-renderSummary :: String -> String -> String           -> String        -> Int    -> T.Text
+renderSummary :: String -> String -> String           -> String        -> Int    -> Builder
 renderSummary    fileName  stats     svgGraphWithoutRefs svgGraphWithRefs graphNo =
-    renderGraphWithToggle (T.pack svgGraphWithoutRefs) (T.pack svgGraphWithRefs) graphNo <>
-    renderStats (T.pack stats) <>
-    renderLinks (T.pack fileName)
+    renderGraphWithToggle svgGraphWithoutRefs svgGraphWithRefs graphNo <>
+    renderStats stats <>
+    renderLinks fileName
 
-
-compileFragments :: [ String ] -> [ ClaferMode ] -> Either [ClaferErr] (Map.Map ClaferMode CompilerResult)
-compileFragments    fragments     claferModes    =
+compileFragments :: [String] -> [ClaferMode] -> Either [ClaferErr] (Map.Map ClaferMode CompilerResult)
+compileFragments    fragments   claferModes   =
     -- compile all clafer code
     runClafer defaultClaferArgs{
                 mode=claferModes,
                 keep_unused=True,
                 add_comments=True,
-                show_references=False } $ do
-                                            mapM_ addModuleFragment fragments
-                                            parse
-                                            iModule <- desugar Nothing
-                                            compile iModule
-                                            generate
+                show_references=False } $
+        do
+            mapM_ addModuleFragment fragments
+            parse
+            iModule <- desugar Nothing
+            compile iModule
+            generate
 
 
 renderAnalyzeWithClaferMooViz :: String -> String -> String -> Block
 renderAnalyzeWithClaferMooViz fileName serverURL serverPort =
-    RawBlock "html" $ T.pack (unlines [
-      "<div>" ++
-      "<a href=\"http://" ++ serverURL ++ ":8092/?claferFileURL=http://" ++ serverURL ++ ":" ++ serverPort ++ "/clafer/" ++
-      fileName ++
-      ".cfr\" target=\"_blank\" " ++
-      "style=\"background-color: #ccc;color: white;text-decoration: none;padding: 1px 5px 1px 5px;\" >" ++
-      "Analyze with ClaferMooVisualizer" ++
-      "</a></div><br>\n"
-      ])
+    RawBlock "html" $ toStrict . toLazyText $
+      ("<div>" <> nl <>
+      "<a href=\"http://" <> fromString serverURL <> ":8092/?claferFileURL=http://" <> fromString serverURL <> singleton ':' <> fromString serverPort <> "/clafer/" <> nl <>
+      fromString fileName <> nl <>
+      ".cfr\" target=\"_blank\" " <> nl <>
+      "style=\"background-color: #ccc;color: white;text-decoration: none;padding: 1px 5px 1px 5px;\" >" <> nl <>
+      "Analyze with ClaferMooVisualizer" <> nl <>
+      "</a></div><br>" <> nl :: Builder)
 
 renderConfigureWithClaferConfigurator :: String -> String -> String -> Block
 renderConfigureWithClaferConfigurator fileName serverURL serverPort =
-    RawBlock "html" $ T.pack (unlines [
-      "<div>" ++
-      "<a href=\"http://" ++ serverURL ++ ":8093/?claferFileURL=http://" ++ serverURL ++ ":" ++ serverPort ++ "/clafer/" ++
-      fileName ++
-      ".cfr\" target=\"_blank\" " ++
-      "style=\"background-color: #ccc;color: white;text-decoration: none;padding: 1px 5px 1px 5px;\" >" ++
-      "Configure with ClaferConfigurator" ++
-      "</a></div><br>\n"
-      ])
+    RawBlock "html" $ toStrict . toLazyText $
+      ("<div>" <> nl <>
+      "<a href=\"http://" <> fromString serverURL <> ":8093/?claferFileURL=http://" <> fromString serverURL <> singleton ':' <> fromString serverPort <> "/clafer/" <> nl <>
+      fromString fileName <> nl <>
+      ".cfr\" target=\"_blank\" " <> nl <>
+      "style=\"background-color: #ccc;color: white;text-decoration: none;padding: 1px 5px 1px 5px;\" >" <> nl <>
+      "Configure with ClaferConfigurator" <> nl <>
+      "</a></div><br>\n" :: Builder)
 
 renderAddOpenInIDE :: String -> String -> String -> Block
-renderAddOpenInIDE fileName serverURL serverPort =
-    RawBlock "html" $ T.pack (unlines [
-      "<div>" ++
-      "<a href=\"http://" ++ serverURL ++ ":8094/?claferFileURL=http://" ++ serverURL ++ ":" ++ serverPort ++ "/clafer/" ++
-      fileName ++
-      ".cfr\" target=\"_blank\" " ++
-      "style=\"background-color: #ccc;color: white;text-decoration: none;padding: 1px 5px 1px 5px;\" >" ++
-      "Open in ClaferIDE" ++
-      "</a></div><br>\n"
-      ])
+renderAddOpenInIDE    fileName  serverURL serverPort =
+    RawBlock "html" $ toStrict . toLazyText $
+      ("<div>" <> nl <>
+      "<a href=\"http://" <> fromString serverURL <> ":8094/?claferFileURL=http://" <> fromString serverURL <> singleton ':' <> fromString serverPort <> "/clafer/" <> nl <>
+      fromString fileName <> nl <>
+      ".cfr\" target=\"_blank\" " <> nl <>
+      "style=\"background-color: #ccc;color: white;text-decoration: none;padding: 1px 5px 1px 5px;\" >" <> nl <>
+      "Open in ClaferIDE" <> nl <>
+      "</a></div><br>\n" :: Builder)
 
 getPageName:: PluginM String
 getPageName = replace " " "_" . replace "/" "_" . pgPageName . ctxLayout <$> getContext
